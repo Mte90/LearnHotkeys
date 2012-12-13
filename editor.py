2c998405ab3c99d0188da069c9e5dc9d6c74110f
Mte90 <mte90smanettator@gmail.com>
2012-12-13 11:28:19 +0100

    set text in textbox

--- editor.py
+++ editor.py
@@ -16,49 +16,48 @@ class EditorWindow ( QDialog , Ui_Editor):
 		QDialog.__init__( self, parent )
 		self.ui = Ui_Editor()
 		self.ui.setupUi( self )
-		#self.ui.saveButton.clicked.connect(self.saveHTML)
-		#self.ui.closeButton.clicked.connect(self.accept)
 		for root, dirs, files in os.walk(self.hotkeys_path):
 			for name in files:
 				filename = os.path.join(root, name)
 				self.ui.comboHotkeys.addItem(os.path.basename(filename))
-		#self.ui.themeChooser.currentIndexChanged.connect(self.saveConfig)
-		#self.loadHotkeys()
+		self.ui.comboHotkeys.currentIndexChanged.connect(self.loadHotkeys)
+		self.loadHotkeys()
 		self.show()
 
-	#def loadHotkeys(self):
-		#if sys.version_info < (3, 0):
-			#if self.settings.value('file_name_default').toString() != "":
-				#fname = self.hotkeys_folder+self.settings.value('file_name_default').toString()
-		#else:
-			#if self.settings.value('file_name_default') != "":
-				#fname = self.hotkeys_folder+self.settings.value('file_name_default')
-		#dom = QDomDocument()
-		#error = None
-		#fh = None
-		#try:
-			#fh = QFile(fname)
-			#if not fh.open(QIODevice.ReadOnly):
-				#print(IOError, unicode(fh.errorString()))
-			#if not dom.setContent(fh):
-				#print(ValueError, "could not parse XML")
-		#except (IOError, OSError, ValueError) as e:
-			#error = "Failed to import: {0}".format(e)
-		#finally:
-			#if fh is not None:
-				#fh.close()
-			#if error is not None:
-				#return False, error
-		#root = dom.documentElement()
-		#if not root.hasAttribute('fileversion'):
-			#QMessageBox.information(self.window(), "LearnHotkeys","The file {} is not an LearnHotkeys definition file." % self.settings.value('file_name_default').toString())
-			#return False
-		#self.html_def += root.attribute('software')+" - "+root.attribute('softwareversion')+" - "+root.attribute('def')+"<br>\n<a href='"+root.attribute('softwaresite')+"'>" \
-		#+root.attribute('softwaresite')+"</a><br> CheatSheet version: "+root.attribute('fileversion')+"<br><br>"
-		#child = root.firstChildElement('hotkey')
-		#while not child.isNull():
+	def loadHotkeys(self):
+		fname = self.hotkeys_folder+self.ui.comboHotkeys.currentText()
+		dom = QDomDocument()
+		error = None
+		fh = None
+		try:
+			fh = QFile(fname)
+			if not fh.open(QIODevice.ReadOnly):
+				print(IOError, unicode(fh.errorString()))
+			if not dom.setContent(fh):
+				print(ValueError, "could not parse XML")
+		except (IOError, OSError, ValueError) as e:
+			error = "Failed to import: {0}".format(e)
+		finally:
+			if fh is not None:
+				fh.close()
+			if error is not None:
+				return False, error
+		root = dom.documentElement()
+		if not root.hasAttribute('fileversion'):
+			QMessageBox.information(self.window(), "LearnHotkeys","The file %s is not an LearnHotkeys definition file." % self.ui.comboHotkeys.currentText())
+			return False
+		self.ui.description.setText(root.attribute('def'))
+		self.ui.webSite.setText(root.attribute('softwaresite'))
+		self.ui.fileVersion.setText(root.attribute('fileversion'))
+		self.ui.softwareName.setText(root.attribute('software'))
+		self.ui.softwareVersion.setText(root.attribute('softwareversion'))
+		count_child = 0
+		child = root.firstChildElement('hotkey')
+		while not child.isNull():
 			#self.html_cs += "\n<tr><td>%s</td><td>%s</td></tr>" % (child.firstChildElement('question').text(),child.firstChildElement('key').text())
-			#child = child.nextSiblingElement('hotkey')
+			count_child = count_child+1
+			child = child.nextSiblingElement('hotkey')
+		self.ui.totalQuestion.setText('Total question: '+str(count_child))
 		#self.html_cs += "</table></body></html>"
 		#if sys.version_info < (3, 0):
 			#self.ui.csView.setHtml((self.html_style % self.get_file_content(self.theme_folder+self.settings.value('theme').toString()))+self.html_thead+self.html_cs)
